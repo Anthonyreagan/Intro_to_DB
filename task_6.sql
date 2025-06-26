a433514f62ea2e04557ccab21463045bed1d3817
Anthony <anthonyreagan126@gmail.com>
2025-06-26 04:58:24 +0300

    inserting mutilple data in the database

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Insert multiple rows into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
+
+(5, 'Anthony Simiyu', 'antony@example.com', '126 Happiness Ave.');
+
