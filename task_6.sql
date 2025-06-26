19dc5e70f64aa38b7818302f59fdc2ebca71f332
Anthony <anthonyreagan126@gmail.com>
2025-06-26 05:03:23 +0300

    inserting mutilple data in the database

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,8 @@
--- Use the alx_book_store database
-USE alx_book_store;
+-- task_6.sql
+-- Inserts multiple customers into Customers table with exact address formats
 
--- Insert multiple rows into the Customers table
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.'),
-(5, 'Anthony Simiyu', 'antony@example.com', '126 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
